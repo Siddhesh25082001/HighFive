cda9edc903f7860b4b88937cde3cabd0cab58e7d
merlin-201 <npradeeppatil2001@gmail.com>
2022-04-01 01:46:28 +0530

    renamed mode2.js and created room.js copy

---
+++ public/js/words.js
@@ -0,0 +1,117 @@
+/* -------------------------------------------------------------------------- */
+/*                             CONSTANTS AND DATA                             */
+/* -------------------------------------------------------------------------- */
+
+const IMG_LINK_PREFIX = "https://img.icons8.com/emoji/48/000000/"
+let words = {
+    "yes" : [
+        {
+            text : "yes",
+            icon : "check-mark-emoji.png"
+        },
+    ],
+
+    "no" : [
+        {
+            text : "no",
+            icon : "no-entry-emoji.png"
+        },
+    ],
+
+    "greeting" : [
+        {
+            text : "hello",
+            icon : "victory-hand-emoji.png"
+        },
+    
+        {
+            text : "goodbye",
+            icon : "waving-hand-emoji.png"
+        },
+    
+        {
+            text : "okay",
+            icon : "thumbs-up.png"
+        },
+    
+        {
+            text : "please",
+            icon : "pleading-face.png"
+        },
+    
+        {
+            text : "thanks",
+            icon : "folded-hands-emoji-1.png"
+        },
+    
+        {
+            text : "sorry",
+            icon : "white-flag.png"
+        },
+    ],
+
+    "pronoun" : [
+        {
+            text : "you",
+            icon : "pleading-face.png"
+        },
+    
+        {
+            text : "i am",
+            icon : "pleading-face.png"
+        },
+    ],
+
+    "time" : [  
+        {
+            text : "now",
+            icon : "pleading-face.png"
+        }
+    ],
+
+    "action" : [
+        {
+            text : "make",
+            icon : "pleading-face.png"
+        },
+    ],
+
+    "adjective" : [
+        {
+            text : "good",
+            icon : "pleading-face.png"
+        },
+    ],
+
+    "emoji" : [
+        {
+            text : "uwu",
+            icon : "pleading-face.png"
+        },
+        {
+            text : "UwU",
+            icon : "pleading-face.png"
+        },
+    ],
+
+    "question" : [
+        {
+            text : "why",
+            icon : "pleading-face.png"
+        },
+    ],
+
+    "alphabet" : [
+        {
+            text : "z",
+            icon : "pleading-face.png"
+        },
+    ],
+
+    "number" : [
+        {
+            text : "0",
+            icon : "pleading-face.png"
+        },
+    ]
+}
