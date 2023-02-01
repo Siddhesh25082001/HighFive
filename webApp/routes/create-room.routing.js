1576c8a4f223c7711be62c60a7933c05527e4486
Siddhesh Mane <67231450+Siddhesh25082001@users.noreply.github.com>
2023-02-01 19:55:38 +0530

    Delete webApp directory

--- webApp/routes/create-room.routing.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const createRoomController = require('../controllers/createRoom.controller');
-
-router.get('/',createRoomController.create_room);
-
-module.exports = router;
